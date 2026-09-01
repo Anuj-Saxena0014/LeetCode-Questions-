37652c1696205eba18ab7ad7fee39633453fd288
Anuj Saxena <saxenaanuj2005@gmail.com>
2026-09-02 02:18:14 +0530

    Time: 9 ms (16.2%), Space: 25.9 MB (8.77%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,55 +1,25 @@
 class Solution {
 public:
-    bool validbox(vector<vector<char>>& board, int sr, int er, int sc , int ec){
-        unordered_set<char> s;
-        for(int i =sr; i<= er; i++ ){
-            for(int j = sc; j<= ec;j++){
-               if(board[i][j]== '.' ) continue;
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<string> s;
+        for(int i =0 ; i<9 ; i++){
+            for(int j =0; j<9; j++){
+                if(board[i][j] == '.') continue;
 
-                if(s.find(board[i][j]) != s.end())
-                 return false;
-            
-            s.insert(board[i][j]);
+                string r = string(1, board[i][j]) +"_ROW_" + to_string(i);
+                string c = string(1, board[i][j]) +"_COL_" + to_string(j);
+                string box = string(1, board[i][j]) +"_BOX_" + to_string(i/3) + to_string(j/3);
+
+                if(s.find(r) != s.end()||
+                   s.find(c) != s.end() ||
+                   s.find(box) != s.end() ){
+                    return false;
+                   }
+                   s.insert(r);
+                   s.insert(c);
+                   s.insert(box);
             }
         }
         return true;
-    }
-    bool isValidSudoku(vector<vector<char>>& board) {
-      
-      for(int r =0; r< 9 ; r++){
-        unordered_set<char> s;
-        for(int c =0; c <9; c++){
-            if(board[r][c] == '.') continue;
-
-            if(s.find(board[r][c]) != s.end()) {
-                return false;
-            }
-            s.insert(board[r][c]);
-        }
-      }
-
-      for(int c =0; c< 9 ; c++){
-        unordered_set<char> s;
-        for(int r =0; r <9; r++){
-            if(board[r][c] == '.') continue;
-
-            if(s.find(board[r][c]) != s.end()) {
-                return false;
-            }
-            s.insert(board[r][c]);
-        }
-      }
-      
-      for(int sr =0; sr<9; sr+=3){
-        int er = sr+2;
-        for(int sc =0; sc<9; sc+=3){
-            int ec = sc+2;
-
-            if(!validbox(board,sr,er, sc, ec)){
-                return false;
-            }
-        }
-      }
-    return true;
     }
 };

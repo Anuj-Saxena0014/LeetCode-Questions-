6ec36e6a1aa4d1627cdc8c278854514f154841ad
Anuj Saxena <saxenaanuj2005@gmail.com>
2026-09-12 00:48:01 +0530

    Time: 35 ms (16.56%), Space: 55.1 MB (8.59%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,0);
+        dp[0]=true;
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(dp[j] and j+nums[j]>=i){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
